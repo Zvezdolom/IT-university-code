35a6b377713b0cb994a1ab4b1a3b69cb02fe6405
Vladimir K <vkalininz@mail.ru>
2021-02-25 00:51:02 +0300

    fix version 1.2.0
    Практика № 1
    Задание № 2

--- [01] Практика № 1/[02] Задание № 2/Form1.cs
+++ [01] Практика № 1/[02] Задание № 2/Form1.cs
@@ -16,5 +16,21 @@ namespace _02__Задание___2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.AntiqueWhite;
+            label1.Text = "Начало работы";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Привет";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "До свидания";
+        }
     }
 }
