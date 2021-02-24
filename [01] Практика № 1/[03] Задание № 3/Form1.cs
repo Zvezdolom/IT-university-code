67cebc8bb869468a6df7215c7383af4f8565dd18
Vladimir K <vkalininz@mail.ru>
2021-02-25 00:51:25 +0300

    fix version 1.3.0
    Практика № 1
    Задание № 3

--- [01] Практика № 1/[03] Задание № 3/Form1.cs
+++ [01] Практика № 1/[03] Задание № 3/Form1.cs
@@ -16,5 +16,21 @@ namespace _03__Задание___3
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.AntiqueWhite;
+            label1.Text = "Начало работы";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+        }
     }
 }
