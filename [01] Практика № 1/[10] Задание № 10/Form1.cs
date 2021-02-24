c70378e61b2b3574fc86d0a6859acb6782546df3
Vladimir K <vkalininz@mail.ru>
2021-02-25 00:54:39 +0300

    fix version 1.10.0
    Практика № 1
    Задание № 10

--- [01] Практика № 1/[10] Задание № 10/Form1.cs
+++ [01] Практика № 1/[10] Задание № 10/Form1.cs
@@ -16,5 +16,26 @@ namespace _10__Задание___10
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.AntiqueWhite;
+            textBox1.Text = "Готов к работе";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "*****";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "+++++";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "00000";
+        }
     }
 }
