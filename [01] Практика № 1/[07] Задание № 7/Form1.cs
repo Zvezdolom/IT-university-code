30af455447ba47e46139b1c6522aaecc15c3ef49
Vladimir K <vkalininz@mail.ru>
2021-02-25 00:53:27 +0300

    fix version 1.7.0
    Практика № 1
    Задание № 7

--- [01] Практика № 1/[07] Задание № 7/Form1.cs
+++ [01] Практика № 1/[07] Задание № 7/Form1.cs
@@ -16,5 +16,36 @@ namespace _07__Задание___7
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label4.Visible = false;
+
+            label1.Visible = true;
+            label3.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = false;
+
+            label1.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label4.Visible = false;
+
+            label2.Visible = true;
+            label3.Visible = true;
+        }
     }
 }
