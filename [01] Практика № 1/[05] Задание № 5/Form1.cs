38b73873a1ddc4b561cbd597b5b6d9261cb60e21
Vladimir K <vkalininz@mail.ru>
2021-02-25 00:52:21 +0300

    fix version 1.5.0
    Практика № 1
    Задание № 5

--- [01] Практика № 1/[05] Задание № 5/Form1.cs
+++ [01] Практика № 1/[05] Задание № 5/Form1.cs
@@ -16,5 +16,21 @@ namespace _05__Задание___5
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.AntiqueWhite;
+            textBox1.Text = "+++++";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "*****";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
     }
 }
