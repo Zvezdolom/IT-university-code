fa5e1acd25f575114c37485d4ce187e754e05be8
Vladimir K <vkalininz@mail.ru>
2021-02-25 00:55:42 +0300

    fix version 1.14.0
    Практика № 1
    Задание № 14

--- [01] Практика № 1/[14] Задание № 14/Form1.cs
+++ [01] Практика № 1/[14] Задание № 14/Form1.cs
@@ -16,5 +16,51 @@ namespace _14__Задание___14
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Text = "1";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Text = "1";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Text = "1";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Text = "Bomb";
+            this.BackColor = Color.Red;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button6.Text = "1";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            button7.Text = "1";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            button8.Text = "1";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            button9.Text = "1";
+        }
     }
 }
