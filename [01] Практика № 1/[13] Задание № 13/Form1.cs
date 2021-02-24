55c3c2f2b1b7470488fda0afc2dd3ba5e5853fe4
Vladimir K <vkalininz@mail.ru>
2021-02-25 00:55:27 +0300

    fix version 1.13.0
    Практика № 1
    Задание № 13

--- [01] Практика № 1/[13] Задание № 13/Form1.cs
+++ [01] Практика № 1/[13] Задание № 13/Form1.cs
@@ -16,5 +16,23 @@ namespace _13__Задание___13
         {
             InitializeComponent();
         }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.AntiqueWhite;
+            textBox1.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+        }
     }
 }
