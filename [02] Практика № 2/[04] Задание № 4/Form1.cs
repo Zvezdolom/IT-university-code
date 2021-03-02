85d07f60c34acb71ae62c1d96baf313de416b5c3
Vladimir K <vkalininz@mail.ru>
2021-03-03 02:58:33 +0300

    fix version 2.4.0
    Практика № 2
    Задание № 4

--- [02] Практика № 2/[04] Задание № 4/Form1.cs
+++ [02] Практика № 2/[04] Задание № 4/Form1.cs
@@ -16,5 +16,19 @@ namespace _04__Задание___4
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "Практическая работа №2 Задание №4 Калинин В. Н.";
+            textBox2.Text += Environment.NewLine + "Рассчитать значение выражения ......";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            textBox2.Text += Environment.NewLine + "При x = " + x.ToString();
+            double y = (Math.Cos(Math.Sqrt(x + 1)) / (2 * Math.Abs(1 - Math.Sqrt(x)) + 3 * (1 + Math.Pow(Math.E, x)) / Math.Pow(2, x + 1) - 1));
+            textBox2.Text += Environment.NewLine + "Результат y = " + y.ToString();
+        }
     }
 }
