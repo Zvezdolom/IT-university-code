52800517172306e27224e2bb10b7d76d07850d00
Vladimir K <vkalininz@mail.ru>
2021-03-03 02:58:06 +0300

    fix version 2.2.0
    Практика № 2
    Задание № 2

--- [02] Практика № 2/[02] Задание № 2/Form1.cs
+++ [02] Практика № 2/[02] Задание № 2/Form1.cs
@@ -16,5 +16,19 @@ namespace _02__Задание___2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "Практическая работа №2 Задание №2 Калинин В. Н.";
+            textBox2.Text += Environment.NewLine + "Рассчитать значение выражения ......";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            textBox2.Text += Environment.NewLine + "При x = " + x.ToString();
+            double y = (0.5 * x * x - 1) / (Math.Sqrt(x)) + (Math.Abs(10 - Math.Pow(Math.E, 0.5 * x * x + 1))) / (Math.Log(2 * x, 10) - 1);
+            textBox2.Text += Environment.NewLine + "Результат y = " + y.ToString();
+        }
     }
 }
