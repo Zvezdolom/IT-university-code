86169f48be04c110f1dbc0b5dc3ad9ae9ef7b845
Vladimir K <vkalininz@mail.ru>
2021-03-03 02:59:25 +0300

    fix version 2.7.0
    Практика № 2
    Задание № 7

--- [02] Практика № 2/[07] Задание № 7/Form1.cs
+++ [02] Практика № 2/[07] Задание № 7/Form1.cs
@@ -16,5 +16,19 @@ namespace _07__Задание___7
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "Практическая работа №2 Задание №7 Калинин В. Н.";
+            textBox2.Text += Environment.NewLine + "Рассчитать значение выражения ......";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            textBox2.Text += Environment.NewLine + "При x = " + x.ToString();
+            double y = (2*Math.Log(x)*Math.Cos(2*x)-3)*(Math.Pow(x+1,2)/(x-1));
+            textBox2.Text += Environment.NewLine + "Результат y = " + y.ToString();
+        }
     }
 }
