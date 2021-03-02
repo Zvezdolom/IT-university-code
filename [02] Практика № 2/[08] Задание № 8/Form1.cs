f4c1de4ddfa8680ccd7554b99f61908b3f1b97e6
Vladimir K <vkalininz@mail.ru>
2021-03-03 02:59:42 +0300

    fix version 2.8.0
    Практика № 2
    Задание № 8

--- [02] Практика № 2/[08] Задание № 8/Form1.cs
+++ [02] Практика № 2/[08] Задание № 8/Form1.cs
@@ -16,5 +16,19 @@ namespace _08__Задание___8
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "Практическая работа №2 Задание №8 Калинин В. Н.";
+            textBox2.Text += Environment.NewLine + "Рассчитать значение выражения ......";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            textBox2.Text += Environment.NewLine + "При x = " + x.ToString();
+            double y = (3 * (x * x - Math.Abs(3 * x) + Math.Abs(1 - Math.Cos(x * x)))) / (2 * Math.Pow(Math.E, 3 * x - 2) - 3 * ((2 + x) / x));
+            textBox2.Text += Environment.NewLine + "Результат y = " + y.ToString();
+        }
     }
 }
