6768c6de6c95f7656a6b01ff5ac9648f4e971de4
Vladimir K <vkalininz@mail.ru>
2021-03-03 03:11:16 +0300

    fix version 3.12.0
    Практика № 3
    Задание № 12

--- [03] Практика № 3/[12] Задание № 12/Form1.cs
+++ [03] Практика № 3/[12] Задание № 12/Form1.cs
@@ -16,5 +16,54 @@ namespace _12__Задание___12
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int SelectedItem = new int();
+            double Answer = new int();
+            double Z = new int();
+
+            double x = Convert.ToDouble(textBox1.Text);
+            double y = Convert.ToDouble(textBox2.Text);
+            double z = Convert.ToDouble(textBox3.Text);
+
+            textBox4.Text = "Результаты работы программы ст. Калинина В.Н. " + Environment.NewLine;
+
+            textBox4.Text += "При X = " + textBox1.Text + Environment.NewLine;
+            textBox4.Text += "При Y = " + textBox2.Text + Environment.NewLine;
+            textBox4.Text += "При Z = " + textBox3.Text + Environment.NewLine;
+
+            if (radioButton1.Checked)
+                SelectedItem = 1;
+            if (radioButton2.Checked)
+                SelectedItem = 2;
+            if (radioButton3.Checked)
+                SelectedItem = 3;
+
+            switch (SelectedItem)
+            {
+                case 1:
+                    Z = Math.Sinh(z);
+                    break;
+                case 2:
+                    Z = Math.Cosh(z);
+                    break;
+                case 3:
+                    Z = Math.Exp(z);
+                    break;
+                default:
+                    textBox4.Text += "Решение не найдено" + Environment.NewLine;
+                    return;
+            }
+
+            if (z >= 3)
+                Answer = Z - 2;
+            if ((-2 < z) && (z < 3))
+                Answer = 0.5 * Z;
+            if (z <= -2)
+                Answer = -2;
+
+            textBox4.Text += "y = " + Answer.ToString() + Environment.NewLine;
+        }
     }
 }
