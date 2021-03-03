a6b7473a1f1f634d99ca0e4f46436bf75c834d37
Vladimir K <vkalininz@mail.ru>
2021-03-03 03:00:16 +0300

    fix version 2.10.0
    Практика № 2
    Задание № 10

--- [02] Практика № 2/[10] Задание № 10/Form1.cs
+++ [02] Практика № 2/[10] Задание № 10/Form1.cs
@@ -16,5 +16,19 @@ namespace _10__Задание___10
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "Практическая работа №2 Задание №10 Калинин В. Н.";
+            textBox2.Text += Environment.NewLine + "Рассчитать значение выражения ......";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            textBox2.Text += Environment.NewLine + "При x = " + x.ToString();
+            double y = (Math.Abs(1+Math.Cos(Math.Sqrt(x)))-Math.Pow(Math.Log10(2*x),2))/(Math.Sqrt(3*x+4)-Math.Pow(2,0.5*x-1));
+            textBox2.Text += Environment.NewLine + "Результат y = " + y.ToString();
+        }
     }
 }
