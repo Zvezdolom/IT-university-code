7df10e836b00aebd1b3924adf956ff02e1414296
Vladimir K <vkalininz@mail.ru>
2021-03-03 03:14:38 +0300

    fix version 4.3.0
    Практика № 4
    Задание № 3

--- [04] Практика № 4/[03] Задание № 3/Form1.cs
+++ [04] Практика № 4/[03] Задание № 3/Form1.cs
@@ -16,5 +16,51 @@ namespace _03__Задание___3
         {
             InitializeComponent();
         }
+        public static double Factorial(double x)
+        {
+            if (x == 0)
+                return 1;
+            return x * Factorial(x - 1);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Sum = 0;
+            int SelectedItem = new int();
+            double Part = new double();
+            double Iteration = 5;
+
+            double Range = Convert.ToDouble(textBox1.Text);
+
+            if (radioButton1.Checked == radioButton2.Checked)
+            {
+                textBox2.Text = "Пожалуйста выберите действие" + Environment.NewLine;
+                return;
+            }
+
+            textBox2.Text = "Результаты работы программы ст. Калинина В.Н. " + Environment.NewLine;
+            textBox2.Text += "При Range = " + textBox1.Text + Environment.NewLine;
+
+            if (radioButton1.Checked)
+                SelectedItem = 1;
+            if (radioButton2.Checked)
+                SelectedItem = 2;
+
+            switch (SelectedItem)
+            {
+                case 1:
+                    Part = Math.Sqrt(Iteration + 2) / (1 + Math.Sqrt(Factorial(Iteration + 1)));
+                    while (Part >= Range)
+                    {
+                        Part = Math.Sqrt(Iteration + 2) / (1 + Math.Sqrt(Factorial(Iteration + 1)));
+                        Sum += Part;
+                        Iteration++;
+                    }
+                    textBox2.Text += "Расчет суммы ряда S = " + Convert.ToString(Sum) + Environment.NewLine;
+                    break;
+                case 2:
+                    textBox2.Text += "Для данного выражения можно посчитать только сумму ряда S" + Environment.NewLine;
+                    break;
+            }
+        }
     }
 }
