4eee3d2767fd953bd85a05b1cf46e40bd4a1c620
Vladimir K <vkalininz@mail.ru>
2021-03-03 03:17:18 +0300

    fix version 4.13.0
    Практика № 4
    Задание № 13

--- [04] Практика № 4/[13] Задание № 13/Form1.cs
+++ [04] Практика № 4/[13] Задание № 13/Form1.cs
@@ -16,5 +16,49 @@ namespace _13__Задание___13
         {
             InitializeComponent();
         }
+        public static double Factorial(double x)
+        {
+            if (x == 0)
+                return 1;
+            return x * Factorial(x - 1);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Multiplication = 1;
+            int SelectedItem = new int();
+            double Part = new double();
+            double Iteration = 0;
+
+            double Range = Convert.ToDouble(textBox1.Text);
+
+            if (radioButton1.Checked == radioButton2.Checked)
+            {
+                textBox2.Text = "Пожалуйста выберите действие" + Environment.NewLine;
+                return;
+            }
+
+            textBox2.Text = "Результаты работы программы ст. Калинина В.Н. " + Environment.NewLine;
+            textBox2.Text += "При Range = " + textBox1.Text + Environment.NewLine;
+
+            if (radioButton1.Checked)
+                SelectedItem = 1;
+            if (radioButton2.Checked)
+                SelectedItem = 2;
+
+            switch (SelectedItem)
+            {
+                case 1:
+                    textBox2.Text += "Для данного выражения можно посчитать только произведения ряда P" + Environment.NewLine;
+                    break;
+                case 2:
+                    for (; Iteration <= Range; Iteration++)
+                    {
+                        Part = (Factorial(Iteration + 2)) / (Iteration + 1);
+                        Multiplication *= Part;
+                    }
+                    textBox2.Text += "Расчет произведения ряда P = " + Convert.ToString(Multiplication) + Environment.NewLine;
+                    break;
+            }
+        }
     }
 }
