ac6a76b8fadf504e9c251351e95ba0cc59c2b70e
Vladimir K <vkalininz@mail.ru>
2021-03-03 03:00:02 +0300

    fix version 2.9.0
    Практика № 2
    Задание № 9

--- [02] Практика № 2/[09] Задание № 9/Form1.cs
+++ [02] Практика № 2/[09] Задание № 9/Form1.cs
@@ -16,5 +16,19 @@ namespace _09__Задание___9
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "Практическая работа №2 Задание №9 Калинин В. Н.";
+            textBox2.Text += Environment.NewLine + "Рассчитать значение выражения ......";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            textBox2.Text += Environment.NewLine + "При x = " + x.ToString();
+            double y = (Math.Sin(3*x)+Math.Pow(Math.E,1+x*x))/(Math.Pow(2,x+1)*x+1-Math.Pow(2,0.5*x-1));
+            textBox2.Text += Environment.NewLine + "Результат y = " + y.ToString();
+        }
     }
 }
