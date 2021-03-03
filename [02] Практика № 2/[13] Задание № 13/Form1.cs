64fc40fefc17495ebc9735ed6bd651eee256e9ff
Vladimir K <vkalininz@mail.ru>
2021-03-03 03:01:08 +0300

    fix version 2.13.0
    Практика № 2
    Задание № 13

--- [02] Практика № 2/[13] Задание № 13/Form1.cs
+++ [02] Практика № 2/[13] Задание № 13/Form1.cs
@@ -16,5 +16,19 @@ namespace _13__Задание___13
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "Практическая работа №2 Задание №13 Калинин В. Н.";
+            textBox2.Text += Environment.NewLine + "Рассчитать значение выражения ......";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            textBox2.Text += Environment.NewLine + "При x = " + x.ToString();
+            double y = (2*Math.Log10(Math.Sqrt(x))-3*((x+1)/(2*x+3)))/(Math.Pow(2*x,2)+Math.Abs(Math.Pow(Math.E,2*x)-3*x*x));
+            textBox2.Text += Environment.NewLine + "Результат y = " + y.ToString();
+        }
     }
 }
