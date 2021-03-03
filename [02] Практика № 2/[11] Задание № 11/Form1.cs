90a2e4a5c32ac679f96b1fcaf2461985e42a2e21
Vladimir K <vkalininz@mail.ru>
2021-03-03 03:00:35 +0300

    fix version 2.11.0
    Практика № 2
    Задание № 11

--- [02] Практика № 2/[11] Задание № 11/Form1.cs
+++ [02] Практика № 2/[11] Задание № 11/Form1.cs
@@ -16,5 +16,19 @@ namespace _11__Задание___11
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "Практическая работа №2 Задание №11 Калинин В. Н.";
+            textBox2.Text += Environment.NewLine + "Рассчитать значение выражения ......";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            textBox2.Text += Environment.NewLine + "При x = " + x.ToString();
+            double y = (Math.Sqrt(3 + Math.Log(x, 10) + 15 - x)) / (1 + Math.Sin((2 + x * x) / (1 + x)));
+            textBox2.Text += Environment.NewLine + "Результат y = " + y.ToString();
+        }
     }
 }
