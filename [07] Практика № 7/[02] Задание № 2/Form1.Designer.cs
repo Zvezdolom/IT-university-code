5310c34fcf9ffae85d765695f4ac7f5a194dd17e
Vladimir K <vkalininz@mail.ru>
2021-03-31 07:02:23 +0300

    fix version 7.2.0
    Практика № 7
    Задание № 2

--- [07] Практика № 7/[02] Задание № 2/Form1.Designer.cs
+++ [07] Практика № 7/[02] Задание № 2/Form1.Designer.cs
@@ -28,13 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(454, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 31);
+            this.label2.TabIndex = 22;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(457, 302);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(331, 60);
+            this.button2.TabIndex = 21;
+            this.button2.TabStop = false;
+            this.button2.Text = "Выполнить Задание";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(457, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(331, 89);
+            this.button1.TabIndex = 20;
+            this.button1.TabStop = false;
+            this.button1.Text = "Сгенерировать матрицу N размерности";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(497, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 38);
+            this.textBox2.TabIndex = 19;
+            this.textBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(454, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 31);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "N";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(431, 349);
+            this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 375);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Задание № 2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- [07] Практика № 7/[02] Задание № 2/Form1.cs
+++ [07] Практика № 7/[02] Задание № 2/Form1.cs
@@ -15,6 +15,81 @@ namespace _02__Задание___2
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            button2.Enabled = false;
+        }
+
+        public static int N = new int();
+        public static int[,] Array;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Значение 'N' - пустое", "Error");
+                return;
+            }
+            try
+            {
+                N = Convert.ToInt32(textBox2.Text);
+                if (N <= 0 && N >= 16)
+                {
+                    MessageBox.Show("Пожалуйста вводите цифры в диапозоне от 1 до 15", "Error");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Пожалуйста вводите только цифры", "Error");
+                return;
+            }
+
+            Random random = new Random();
+
+            Array = new int[N, N];
+
+            dataGridView1.RowCount = N;
+            dataGridView1.ColumnCount = N;
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Array[i, j] = random.Next(-99, 100);
+                    dataGridView1.Rows[i].Cells[j].Value = Convert.ToString(Array[i, j]);
+                }
+            }
+
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int Max = int.MinValue; int Min = int.MaxValue;
+            for (int i = 0; i < N - 1; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    if (i + j == N - 1)
+                    {
+                        if (Max < Array[i, j])
+                        {
+                            Max = Array[i, j];
+                        }
+                        if (Min > Array[i, j])
+                        {
+                            Min = Array[i, j];
+                        }
+                    }
+                        
+                }
+            }
+            label2.Text = $"Max элемент = {Max}, \nMin элемент = {Min}, ";
+
+            button1.Enabled = true;
+            button2.Enabled = false;
         }
     }
 }
