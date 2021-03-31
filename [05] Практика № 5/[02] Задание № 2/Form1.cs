4ef402f684c9aeb631c1b5624c25afa7dd93cedd
Vladimir K <vkalininz@mail.ru>
2021-03-31 06:46:10 +0300

    fix version 5.2.0
    Практика № 5
    Задание № 2

--- [05] Практика № 5/[02] Задание № 2/Form1.cs
+++ [05] Практика № 5/[02] Задание № 2/Form1.cs
@@ -16,5 +16,49 @@ namespace _02__Задание___2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            if (index == -1)
+            {
+                MessageBox.Show("Please select the line", "Error");
+                return;
+            }
+            string str = listBox1.Items[index].ToString();
+
+            int count = 0;
+
+            for (int i = 0; i < str.Length-1; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    count++;
+                }
+            }
+
+            label1.Text = "Пробелов = " + count.ToString() + " из " + str.Length + " симв.";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Textbox is empty", "Error");
+                return;
+            }
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            if (index == -1)
+            {
+                MessageBox.Show("Please select the line", "Error");
+                return;
+            }
+            listBox1.Items.RemoveAt(index);
+        }
     }
 }
